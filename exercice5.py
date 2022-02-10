d68f07ebeb4263c03cfddf4b5df14701b9d158b9
koueAnicet <68559596+koueAnicet@users.noreply.github.com>
2022-02-10 15:26:22 +0000

    Update exercice5.py
    le script final de exercice5

--- exercice5.py
+++ exercice5.py
@@ -6,3 +6,28 @@ et simuler la remise de la monnaie en affichant les textes "10 Euros", "5 Euros"
 autant de fois qu’il y a de coupures de chaque sorte à rendre.
 
 """
+
+prixArticle=1
+sommeDue=0
+while prixArticle !=0:
+    prixArticle = int(input("enter le montant de l'article: \t"))
+    sommeDue += prixArticle
+    break
+print("Vous devez :\t", sommeDue," euros.")
+montantVersé =int(input("entez  montant versé"))
+Reste = montantVersé - sommeDue
+Nmb10E =0
+print(Reste)
+while Reste >=10:
+    Nmb10E +=1
+    Reste -=10
+Nmb5E =0
+
+if Reste >=5:
+    Nmb5E =1 
+    Reste -= 5
+    print(Reste)
+print("Rendu de la monnaie :\n") 
+print("Billets de 10 Euros : ", Nmb10E, "\n") 
+print("Billets de 5 Euros : ", Nmb5E, "\n") 
+print("Pièces de 1 Euros : ", Reste, "\n") 
